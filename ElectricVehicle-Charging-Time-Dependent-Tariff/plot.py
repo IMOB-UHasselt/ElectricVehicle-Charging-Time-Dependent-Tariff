60bbd3125859eaeaaa5df12f7376c7dac76d5678
jan <jan.vuurstaek@uhasselt.be>
2014-10-09 10:55:35 +0200

    Updated download links in guides. Plot python script provided with a progress counter.

--- ElectricVehicle-Charging-Time-Dependent-Tariff/plot.py
+++ ElectricVehicle-Charging-Time-Dependent-Tariff/plot.py
@@ -13,13 +13,20 @@ if len(sys.argv) == 1:
 	processPlot= subprocess.Popen(bashCommandPlot, shell=True)
 	outputPlot = processPlot.communicate()[0]
 	
+	dirsCounter = 0
+	dirsLength = -1
 	for root, dirs, files in os.walk("Data/evcwidrs/generated/battSocVisual"):
+		dirsCounter = dirsCounter + 1
+		if dirsLength == -1:
+			dirsLength = len(dirs)
+		sys.stdout.write("\r%d%%" % (dirsCounter*100/dirsLength))
+		sys.stdout.flush()
 		for name in files:
 			if name.endswith(".plt"):
 				bashCommandPlot = "gnuplot " + root + "/" + name
 				processPlot= subprocess.Popen(bashCommandPlot, shell=True)
 				outputPlot = processPlot.communicate()[0]
-	
+	print ("")
 	
 else:
 	print ("Usage: python plot.py")
